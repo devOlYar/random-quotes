e1bd129cb7ed009848d7a0a1a2d55a2623db86ce
Oleksandr Yaroshenko <oleksandr.yaroshenko.developer@gmail.com>
2025-01-17 17:55:39 +0200

    Splitting app into the JS module

---
+++ quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+  {
+    quote: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful',
+    author: 'Albert Schweitzer',
+  },
+  {
+    quote: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    quote: 'Life is 10% what happens to us and 90% how we react to it',
+    author: 'Charles R. Swindoll',
+  },
+  {
+    quote:
+      'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment',
+    author: 'Buddha',
+  },
+  {
+    quote: 'The purpose of our lives is to be happy',
+    author: 'Dalai Lama',
+  },
+  {
+    quote: 'You miss 100% of the shots you don’t take',
+    author: 'Wayne Gretzky',
+  },
+  {
+    quote: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'What lies behind us and what lies before us are tiny matters compared to what lies within us',
+    author: 'Ralph Waldo Emerson',
+  },
+];
+
+export default quotes;

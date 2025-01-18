ff0b44fefc94a872083de94608771215caac94ba
Oleksandr Yaroshenko <oleksandr.yaroshenko.developer@gmail.com>
2025-01-18 17:28:48 +0200

    Show favorite quotes below current quote

--- quotes.js
+++ quotes.js
@@ -2,49 +2,60 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Innovation distinguishes between a leader and a follower',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: "Your time is limited, so don't waste it living someone else's life",
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful',
     author: 'Albert Schweitzer',
+    isFavorite: false,
   },
   {
     quote: 'The best way to predict the future is to invent it',
     author: 'Alan Kay',
+    isFavorite: false,
   },
   {
     quote: 'Life is 10% what happens to us and 90% how we react to it',
     author: 'Charles R. Swindoll',
+    isFavorite: false,
   },
   {
     quote:
       'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment',
     author: 'Buddha',
+    isFavorite: false,
   },
   {
     quote: 'The purpose of our lives is to be happy',
     author: 'Dalai Lama',
+    isFavorite: false,
   },
   {
     quote: 'You miss 100% of the shots you don’t take',
     author: 'Wayne Gretzky',
+    isFavorite: false,
   },
   {
     quote: 'In the middle of every difficulty lies opportunity',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote:
       'What lies behind us and what lies before us are tiny matters compared to what lies within us',
     author: 'Ralph Waldo Emerson',
+    isFavorite: false,
   },
 ];
 
